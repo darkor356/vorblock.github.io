d813ee3cfc8fc28670e57a480067a4ede4e27220
darkorb456 <jaeden532007@outlook.com>
2024-12-08 13:11:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example of a small JS functionality to smooth scroll to sections.
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
